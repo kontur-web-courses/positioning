225297f05f9da77df59ec4386e64eb0f05d60f7c
Coll4pse <Coll4pse@users.noreply.github.com>
2020-10-26 20:39:42 +0500

    Tasks 1-4 done

--- index.js
+++ index.js
@@ -4,4 +4,10 @@
     const element = document.querySelector('.myElement');
     element.style.color = 'red';
     element.style.width = '300px';
-*/
+*/  
+const element = document.querySelector('.front');
+
+let i = 0;
+setInterval(()=>{if(i <= 100){
+    element.style.clipPath = `inset(0 0 0 ${i}%)`
+i = i + 4;  }},200);
