336edc6ed08fe0490a735878cc1b0fe211956053
Nikita <nikitavedeshkin3@gmail.com>
2020-10-26 21:19:19 +0500

    Done 1-4

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+const progressBar = document.querySelector('.red');
+const overlay = document.getElementById('overlay');
+const lightbox = document.getElementById('lightbox');
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function openModal() {
+    overlay.hidden = false;
+    lightbox.hidden = false;
+    setTimeout(() => {
+        let i = 0;
+        let stop = setInterval(() => {
+            if (i <= 1000 && !lightbox.hidden)
+                progressBar.style.width = i++ / 10 + '%';
+            else clearInterval(stop);
+        }, 3);
+    }, 250);
+}
+
+function closeModal() {
+    overlay.hidden = true;
+    lightbox.hidden = true;
+    progressBar.style.width = '0';
+}
