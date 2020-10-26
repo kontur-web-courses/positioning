90f3d03c8443f05d66aa42369a6ed0ff96f779f3
shatilov-makar <62944516+shatilov-makar@users.noreply.github.com>
2020-10-26 20:49:05 +0500

    1com

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function closeAd()
+{
+    const modalDiv = document.getElementById('modal');
+    const overlayDiv = document.getElementById('overlay');
+    modalDiv.style.display = 'none';
+    overlayDiv.style.display = 'none';
+}
+
+
+
