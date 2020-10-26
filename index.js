86793d04861acbc3444030184527575912713653
dima2014t <dima2014t@gmail.com>
2020-10-26 22:15:38 +0500

    Тарасов Бобылев

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
-
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function move() {
+    let elem = document.querySelector('.positive');
+    let elemneg = document.querySelector(".negative");
+    let width = 0;
+    let widthneg = 100;
+    let id = setInterval(frame, 1);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width += 0.16;
+        widthneg -= 0.16;
+        elem.style.width = width + "%";
+        elemneg.style.width = widthneg + "%";
+      }
+    }
+  }
+  
+  move()
