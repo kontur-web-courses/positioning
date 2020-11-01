8ffce9d272a620a7f1af35a39a0ee07c52dc2eef
OlyaLog <olgaaloginova@yandex.ru>
2020-11-01 18:15:15 +0500

    Немытова, Логинова

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+let advanceProgress = ( percent ) => {
+	document.querySelector('.progressBar').style.background = 'linear-gradient(90deg, #F00 ' + percent + '%, rgb( 163, 163, 163 ) ' + percent + '%)';
+	document.querySelector('.progressText').style.backgroundImage = 'linear-gradient(90deg, #FFF ' + percent + '%, rgb( 0, 0, 0) ' + percent + '%)';
+}
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+let percentage = 0;
+let percentageInterval = setInterval(() => {
+	percentage++;
+	if( percentage <= 100 ){
+		advanceProgress( percentage );
+	}
+}, 30 );
