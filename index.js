ab50f14b1a80bc9a522085fefd6b5216121e8990
Dan <dan.mok999@yandex.ru>
2020-11-09 19:46:56 +0500

    Progressbar animation done

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const closeLightboxBtn = document.getElementById('closeLightboxBtn');
+const lightBox = document.getElementById('lightbox');
+const hiddenBox = document.getElementById('hidden');
+const progressValueStyle = document.getElementById('progress-value').style;
+
+fillProgress(3000);
+
+function fillProgress(duration){
+    progressValueStyle.transition = `width ${duration}ms ease-in-out`;
+    progressValueStyle.width = "100%";
+}
+
+closeLightboxBtn.onclick = () => {
+    lightBox.style.display = 'none';
+    hiddenBox.style.display = 'none';
+}
+
 /*
     Изменить элементу цвет и ширину можно вот так:
 
