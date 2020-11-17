6bae7a6a94173481d73306c611c2dba371bf1012
ialmostdeveloper <kill2survive@yandex.ru>
2020-11-17 20:24:17 +0500

    first steps

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay')
+
+
+function closeWindow(){
+    modal.style.display = 'none';
+    // overlay.style.backgroundColor = 'transparent';
+    overlay.style.display = 'none';
+}
+
+function openWindow(){
+    modal.style.display = 'inline-block';
+    // overlay.style.backgroundColor = 'rgba(59, 67, 76, 0.9)';
+    overlay.style.display = 'block';
+}
+
+const closeWindowBtn = document.querySelector('.closeWindowBtn');
+closeWindowBtn.addEventListener('click', closeWindow)
+
+const openWindowBtn = document.querySelector('.openWindowBtn');
+openWindowBtn.addEventListener('click', openWindow)
