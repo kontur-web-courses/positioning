da669e862c32ced4cb6c69bf4b4699af1f19d30d
Кирилл Пастухов <kirillpastuhov@iMac-Kirill.local>
2020-11-21 14:30:25 +0500

    1 - 5

--- index.js
+++ index.js
@@ -5,3 +5,47 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function closeModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('display-none');
+    resetProgressbar();
+}
+
+function openModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('display-none');
+    startProgressbar('#progress');
+}
+
+let timer;
+
+function startProgressbar(id) {
+    const progressbar = document.querySelector(id);
+    const progress = progressbar.querySelector('.progressbar-progress');
+    const progressbarText = progressbar.querySelectorAll('.progressbar-text');
+    
+    const time = 3.5; 
+    let percent = 0;
+
+    cycleAnimation();
+
+    function cycleAnimation() {
+        timer = setTimeout(() => {
+            percent++;
+            progress.style.width = `${percent}%`;
+            progressbarText.forEach (element => {
+                element.textContent=`${percent}%`;
+            })
+
+            if (percent < 100) {
+                cycleAnimation();
+            }
+            
+        }, time * 1000 / 100);
+    }
+}
+
+function resetProgressbar () {
+    clearTimeout(timer);
+}
