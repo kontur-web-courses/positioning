accd4441d41e686c8aca7d1fa7b0d637ad927aa3
iONicod <55252121+iONicod@users.noreply.github.com>
2020-11-24 23:29:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+let closeButton = document.getElementById('closeButton')
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+closeButton.onclick = function () {
+    let overlay = document.getElementById('overlay')
+    let lightBox = closeButton.parentElement;
+    let progressValue = document.querySelector('.progressBar .value')
+    hideElements([closeButton], 0);
+    showLoading(progressValue);
+    hideElements([lightBox, overlay], 3000)
+}
+
+function showLoading(element) {
+    element.style.width = '100%';
+}
+
+function hideElements(elements, delay) {
+    setTimeout(() => {
+        elements.forEach(element => {
+            element.classList.add('hidden')
+        })
+    }, delay)
+}
