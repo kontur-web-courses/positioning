46fedfd87d7b802a0dceaa7dd9545ce4408db60d
ialmostdeveloper <kill2survive@yandex.ru>
2020-11-25 07:39:15 +0500

    animated progressbar

--- index.js
+++ index.js
@@ -17,8 +17,26 @@ function closeWindow(){
 
 function openWindow(){
     modal.style.display = 'inline-block';
-    // overlay.style.backgroundColor = 'rgba(59, 67, 76, 0.9)';
     overlay.style.display = 'block';
+    loadingScale.style.width = '40%'
+    beginAnimation()
+}
+
+function beginAnimation(){
+    let width = 40;
+    animationStep(width)
+}
+
+function animationStep(width){
+    if (width < 100){
+        width++;
+        loadingScale.style.width = `${width}%`
+        setTimeout(()=> animationStep(width), 50)
+    }
+    else{
+        closeWindow()
+        loadingScale.style.width = '100%'
+    }
 }
 
 const closeWindowBtn = document.querySelector('.closeWindowBtn');
@@ -26,3 +44,6 @@ closeWindowBtn.addEventListener('click', closeWindow)
 
 const openWindowBtn = document.querySelector('.openWindowBtn');
 openWindowBtn.addEventListener('click', openWindow)
+
+const loadingScale = document.querySelector('.loading');
+beginAnimation()
