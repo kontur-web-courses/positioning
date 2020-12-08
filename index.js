cc116fd16bcc59b6ea7300a73491bcf6faee037f
newu2012 <newu2011@gmail.com>
2020-12-08 16:07:31 +0500

    Tasks 1-4

--- index.js
+++ index.js
@@ -5,3 +5,29 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+let progressBar
+let overlay
+let lightbox
+
+function openModal() {
+    progressBar = document.querySelector('.red');
+    overlay = document.querySelector('.overlay');
+    lightbox = document.getElementById('lightbox');
+    overlay.hidden = false;
+    lightbox.hidden = false;
+    setTimeout(() => {
+        let i = 0;
+        let stop = setInterval(() => {
+            if (i <= 1000 && !lightbox.hidden)
+                progressBar.style.width = i++ / 10 + '%';
+            else clearInterval(stop);
+        }, 3);
+    }, 250);
+}
+
+function closeModal() {
+    overlay.hidden = true;
+    lightbox.hidden = true;
+    progressBar.style.width = '0';
+}
