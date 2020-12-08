68da94516835d276e63e5ff053a6c46559aafd07
Vlad <mr.paprikus@gmail.com>
2020-12-08 23:14:47 +0500

    1-4 Сенькин В.М. РИ-380012

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const closeModalBtn = document.querySelector('.closeModal');
+const showModalBtn = document.querySelector('.showModal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const frontProgressBar = document.querySelector('.front')
+closeModalBtn.onclick = function (){
+    modal.style.visibility = 'hidden';
+    overlay.style.visibility = 'hidden';
+}
+showModalBtn.onclick = function (){
+    modal.style.visibility = 'visible';
+    overlay.style.visibility = 'visible';
+}
+let start = 0;
+let intervalId = setInterval(function(){
+    if (start > 100) {
+        clearInterval(intervalId);
+    } else {
+        frontProgressBar.style.clipPath = `inset(0 0 0 ${start}%)`;
+    }
+    start++;
+}, 3);
