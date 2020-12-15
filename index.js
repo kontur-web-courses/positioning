d0be69b9da7d31391814cdc723f0bbe8c9cc8a33
Deemjan <orda1500@gmail.com>
2020-12-15 20:10:28 +0500

    1-5 done

--- index.js
+++ index.js
@@ -5,3 +5,61 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay')
+
+
+function closeWindow(){
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+function openWindow(){
+    modal.style.display = 'inline-block';
+    overlay.style.display = 'block';
+}
+
+const closeWindowBtn = document.querySelector('.closeWindowBtn');
+closeWindowBtn.addEventListener('click', closeWindow)
+
+const openWindowBtn = document.querySelector('.openWindowBtn');
+openWindowBtn.addEventListener('click', openWindow);
+
+function animateProgressBar(){
+    document.querySelectorAll('.bar').forEach(progressbar => {
+        let progress = 0
+
+        const intervalId = setInterval(() => {
+            if (progress < 100) {
+                progress++
+                let progressLine = progressbar.firstElementChild
+                progressLine.style.width = `${progress}%`
+            } else {
+                clearInterval(intervalId)
+            }
+        }, 30)
+    })
+}
+
+function expandAccordionItem(){
+    let acc = document.getElementsByClassName("accordion");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            let item = this.nextElementSibling;
+            if (item.style.display === "block") {
+                item.style.display = "none";
+            } else {
+                item.style.display = "block";
+            }
+        });
+    }
+}
+
+animateProgressBar()
+expandAccordionItem()
+
+
