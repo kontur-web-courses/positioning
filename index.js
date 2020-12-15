b29cadda80f3f985cf904af6527da92fd8d46f0d
Dima Goltyapov <43724581+Vasko73232@users.noreply.github.com>
2020-12-15 17:59:45 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+document.querySelector(".closeModal").addEventListener('click', () => {
+    document.querySelector('.modalWindow').style.visibility = 'hidden';
+})
+document.querySelector(".openModal").addEventListener('click', () => {
+    document.querySelector('.modalWindow').style.visibility = 'visible';
+
+})
+
+let width = 0;
+setInterval(() => {
+    showProgress()
+}, 10)
+
+function showProgress() {
+    let redProgressBar = document.querySelector('.redColorProgressBar')
+    redProgressBar.style.clip = `rect(0 ${width+=1}px 40px 0)`
+}
