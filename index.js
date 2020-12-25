36e2586c3aca0d916eb37021f301bcb0da8f4934
Vladimir Podprajtov <vladimirpodpratov@gmail.com>
2020-12-25 21:02:46 +0500

    All task done

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+document.querySelector(".lightboxClose").addEventListener('click', () => {
+    document.querySelector('.modalWindow').style.visibility = 'hidden';
+})
+document.querySelector(".lightboxOpen").addEventListener('click', () => {
+    document.querySelector('.modalWindow').style.visibility = 'visible';
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+})
+
+let width = 0;
+setInterval(() => {
+    showProgress()
+}, 10)
+
+function showProgress() {
+    let redProgressBar = document.querySelector('.redColorProgressBar')
+    redProgressBar.style.clip = `rect(0 ${width+=5}px 40px 0)`
+}
