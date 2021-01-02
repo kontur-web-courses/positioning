72d3de5b46f1319cb17e1db0418be7e797886fcf
Dank005 <daniil.yushkov2000@mail.ru>
2021-01-02 22:32:14 +0500

    1-4 tasks done

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
-
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+let closeButton = document.querySelector(".lightbox-close");
+let lightBox = document.querySelector(".lightbox-container");
+closeButton.addEventListener("click", () => lightBox.remove())
