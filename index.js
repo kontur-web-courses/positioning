2db3e79076d62fd86112f71583a3db0f125ef311
KuroNeko <50028655+DashaDig@users.noreply.github.com>
2021-01-03 16:37:57 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,20 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+function animateProgressBar(){
+    document.querySelectorAll('.bar').forEach(progressbar => {
+        let progress = 0
+
+        const intervalId = setInterval(() => {
+            if (progress < 100) {
+                progress++
+                let progressLine = progressbar.firstElementChild
+                progressLine.style.width = `${progress}%`
+            } else {
+                clearInterval(intervalId)
+            }
+        }, 30)
+    })
+}
+animateProgressBar()
+
