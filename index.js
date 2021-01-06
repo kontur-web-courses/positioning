d986ddddad68262fe433d5ced787d5d0d2062c8f
Roge <ego-R-oge@yandex.ru>
2021-01-06 17:21:14 +0500

    Task done

--- index.js
+++ index.js
@@ -5,3 +5,37 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+
+function closeWindow() {
+  modal.style.display = 'none'
+  // overlay.style.backgroundColor = 'transparent';
+  overlay.style.display = 'none'
+}
+
+function openWindow() {
+  modal.style.display = 'inline-block'
+  // overlay.style.backgroundColor = 'rgba(59, 67, 76, 0.9)';
+  overlay.style.display = 'block'
+}
+
+const closeWindowBtn = document.querySelector('.closeWindowBtn')
+closeWindowBtn.addEventListener('click', closeWindow)
+
+const openWindowBtn = document.querySelector('.openWindowBtn')
+openWindowBtn.addEventListener('click', openWindow)
+
+function setProgress() {
+  const redLine1 = document.querySelector('.progress-bar-red1')
+  const redLine2 = document.querySelector('.progress-bar-red2')
+
+  timeOut(redLine1, 1000)
+  setTimeout(() => timeOut(redLine2, 2000), 1000)
+}
+function timeOut(line, duration) {
+  for (let i = 0; i <= duration; i = i + 50) {
+    setTimeout(() => (line.style.width = i / 10 + 'px'), i)
+  }
+}
+setProgress()
