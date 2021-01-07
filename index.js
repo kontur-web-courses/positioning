1b168a191316d42cbe0abbd4d4dbd3cdae754895
Екатерина Шубинцева <katya.shyb@yandex.ru>
2021-01-07 21:32:40 +0500

    Шубинцева Екатерина 1-5

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+const lightbox = document.querySelector('.lightbox');
+const overlay = document.querySelector('.overlay')
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function closeWindow(){
+    lightbox.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+function openWindow(){
+    lightbox.style.display = 'inline-block';
+    overlay.style.display = 'flex';
+}
+
+const closeLightbox = document.querySelector('.closeLightbox');
+closeLightbox.addEventListener('click', closeWindow)
+
+const openLightbox = document.querySelector('.openLightbox');
+openLightbox.addEventListener('click', openWindow);
+
+function animateProgressBar(){
+    document.querySelectorAll('.bar').forEach(progressbar => {
+        let progress = 0
+
+        const intervalId = setInterval(() => {
+            if (progress < 100) {
+                progress++
+                let progressLine = progressbar.firstElementChild
+                progressLine.style.width = `${progress}%`
+            } else {
+                clearInterval(intervalId)
+            }
+        }, 30)
+    })
+}
+
