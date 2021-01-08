91385c7c0b1d85000ea6c2282b1c883cd22b4e22
Gar1NN <gartas.red@yandex.ru>
2021-01-09 00:47:42 +0500

    1-4 done

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+let progress = document.querySelector(".progress");
+let lightbox = document.querySelector(".lightbox");
+let shadow = document.querySelector(".shadow");
+let closeButton = document.querySelector(".close");
+let width = 600;
+let value = 0;
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+closeButton.addEventListener("click",()=> {
+    lightbox.style.display = 'none';
+    shadow.style.display = 'none';
+})
+
+let progress_bar = setInterval(()=>{
+    value += 3;
+    if (value >= width)
+        clearInterval(progress_bar);
+    progress.style.width = String(value +"px");
+},15);
+
+setTimeout(() => {
+    let p = document.querySelector(".text");
+    p.style.display = "block";
+}, 3000)
+
+function closeLightbox(){
+    
+} 
