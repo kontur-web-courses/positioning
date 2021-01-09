3d0753764d42a858dbbb2c15f010027e140258e6
Алена <worthlessartist@mail.ru>
2021-01-09 20:52:46 +0500

    Task done

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+let advanceProgress = ( percent ) => {
+	document.querySelector('.progress').style.background = 'linear-gradient(90deg, #F00 ' + percent + '%, rgb( 163, 163, 163 ) ' + percent + '%)';
+	document.querySelector('.value').style.backgroundImage = 'linear-gradient(90deg, #FFF ' + percent + '%, rgb( 0, 0, 0) ' + percent + '%)';
+}
+
+let percentage = 0;
+let percentageInterval = setInterval(() => {
+	percentage++;
+	if( percentage <= 100 ){
+		advanceProgress( percentage );
+	}
+}, 30 ); 
