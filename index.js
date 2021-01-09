44e2f8354334d2acc4cd6fe5fb58f5a74e68894e
Sc222 <v.a.shaternikov@gmail.com>
2021-01-09 14:02:46 +0500

    tasks 1-5

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+const modal = document.querySelector('.modal-wrapper');
+const modalProgressbar = document.querySelector('#bar-modal');
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function toggleModal(isVisible) {
+    modal.style.display = isVisible ? "block" : "none";
+    if (isVisible)
+        animateProgressbar();
+}
+
+function animateProgressbar() {
+    let progress = 0;
+    const intervalId = setInterval(() => {
+        if (progress < 100)
+            modalProgressbar.firstElementChild.style.width = `${++progress}%`;
+        else
+            clearInterval(intervalId);
+    }, 25);
+}
+
+function setupAccordions() {
+    document.querySelectorAll(".accordion").forEach(accordion => {
+        accordion.addEventListener("click", function () {
+            this.classList.toggle("active");
+            let item = this.nextElementSibling;
+            item.style.display = item.style.display !== "block" ? "block" : "none";
+        });
+    });
+}
+
+document.querySelector('#close-modal').addEventListener('click', () => toggleModal(false));
+document.querySelector('#open-modal').addEventListener('click', () => toggleModal(true));
+setupAccordions();
