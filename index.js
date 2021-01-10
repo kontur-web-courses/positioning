d490b64325b7ad3bf73f46a93983d66ea64f2204
hedgehog05 <hedgehog05@users.noreply.github.com>
2021-01-10 11:28:19 +0500

    1-5(all)

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 /*
     Изменить элементу цвет и ширину можно вот так:
-
     const element = document.querySelector('.myElement');
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+let progress = document.querySelector(".progress");
+let speed = 200;
+let max = 600;
+let value = 0;
+let progress_bar = setInterval(()=>{
+    value += max/speed;
+    progress.style.width = String(value +"px");
+},3000/speed);
+setTimeout(() => {
+    clearInterval(progress_bar);
+    let p = document.querySelector(".text");
+    p.style.display = "block";
+    let accordion = document.querySelector(".accordion");
+    accordion.style.display = "block";
+}, 3000)
+
+function closeLightbox(){
+    let lightbox = document.querySelector(".lightbox");
+    lightbox.style.display = 'none';
+    let darkening = document.querySelector(".darkening");
+    darkening.style.display = 'none';
+} 
