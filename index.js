0ee9b219df2e4a6d8d313875f24abebf243c4c59
tend3r <drontak6@gmail.com>
2021-01-10 23:56:59 +0500

    Task done, 1-4

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay')
+const progressBar = document.querySelector('.progressValue')
+startProgressBar();
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function startProgressBar() {
+    const maxValue = 100;
+    let value = 0;
+    const time = 3000/maxValue;
+    const loading = function () {
+        value += 1;
+        progressBar.style.width = `${value}%`;
+        if (value >= maxValue) {
+            clearInterval(animate);
+        }
+    }
+    const animate = setInterval(loading, time);
+}
+
+function resetProgressBar() {
+     progressBar.style.width = "0";
+}
+
+function closeWindow(){
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+    resetProgressBar();
+}
+
+function openWindow(){
+    modal.style.display = 'inline-block';
+    overlay.style.display = 'block';
+    startProgressBar();
+}
+
+const closeWindowBtn = document.querySelector('.closeWindowBtn');
+closeWindowBtn.addEventListener('click', closeWindow)
+
+const openWindowBtn = document.querySelector('.openWindowBtn');
+openWindowBtn.addEventListener('click', openWindow)
