3124b71e6c34d89cd4c8c591be0e4b5736f19f2a
kryaazh <57125074+kryaazh@users.noreply.github.com>
2021-04-12 13:51:18 +0500

    1-4

--- index.js
+++ index.js
@@ -5,3 +5,20 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function animateBar() {
+    let progressBar = document.querySelector('.bar');
+
+    let progress = 0;
+    const id = setInterval(() => {
+        if (progress < 100) {
+            progress++;
+            let line = progressBar.firstElementChild;
+            line.style.width = `${progress}%`
+        } else {
+            clearInterval(id);
+        }
+    }, 18)
+}
+
+animateBar();
