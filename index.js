c169cc3371bf9c632853ef31eb18e1893a4e9960
= <=>
2021-04-12 10:46:14 +0500

    Done 4

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+
+const bar = document.querySelector('.progress-bar .bg2');
+bar.style.width = '0%';
+let counter = 0;
+let timer = setInterval(() => {
+	counter += 1;
+	if (counter >= 100)
+		clearInterval(timer);
+	bar.style.width = counter + "%";
+}, 30);
