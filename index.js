ddcdc92ee6033bfa02e98f7c8bf3fc76c2730223
vlad1slavM <46625953+vlad1slavM@users.noreply.github.com>
2021-04-12 14:32:08 +0500

    b

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+
+const element = document.querySelector('.progress_c');
+
+let id = setInterval(frame, 10);
+function frame() {
+    let width = 0;
+    if (width >= 100) {
+        clearInterval(id)
+    }
+    else {
+        width += 10;
+        element.style.width = width + "%";
+    }
+}
+
 /*
     Изменить элементу цвет и ширину можно вот так:
 
@@ -5,3 +20,4 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
