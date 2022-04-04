164e1e6a5e77bdc4991fd34d8f39bea5071851cd
SergeyLiyasov <liseand2002@gmail.com>
2022-04-04 17:59:51 +0500

    автозаполнение прогрес-бара

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function overlay_off() {
     overlay.style.display = 'none';
     lightbox.style.display = 'none';
 }
+
+function run_progress_bar(delta) {
+    const framesCount = 8;
+    const progress_bar = document.querySelector('.progress .bar');
+
+    for (let i = 0; i < framesCount + 1; i++) {
+        setTimeout(function(j) {
+            progress_bar.style.width = `${100 * j / framesCount}%`;
+        }, i * delta / framesCount, i);
+    }
+}
+
+run_progress_bar(500);
