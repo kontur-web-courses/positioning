112c5cf4b9a16277f34fcbd654cbd54041210a54
Alexandra Zaytseva <alexandrazaitzewa@yandex.ru>
2022-04-04 17:25:33 +0500

    lightbox closing

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const lightbox = document.getElementById('lightbox');
+const overlay = document.getElementById('overlay');
+
+function closeLightbox() {
+    lightbox.style.display = 'none';
+    overlay.style.display = 'none';
+}
