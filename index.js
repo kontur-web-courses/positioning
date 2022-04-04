9014274375d09eee99715172327b8b80e9af3aad
artem <2001raf@gmail.com>
2022-04-04 13:01:40 +0500

    1-4 DONE

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+let w = 0;
+let max_w = 320;
+let step = 1;
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function bigger(){
+    if (w >= max_w) clearInterval(_);
+    const element = document.querySelector('.loading-bar');
+    element.style.width = w + 'px';
+    w += step;
+}
+
+let _ = setInterval(bigger, 3000 / (max_w / step));
