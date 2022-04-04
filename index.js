b0e8dfd08444dcd3768a5e28849bc5227f765eb9
AlexMoshkov <9aq360zqmuf1@mail.ru>
2022-04-04 17:57:48 +0500

    add progress bar animation

--- index.js
+++ index.js
@@ -6,12 +6,25 @@
     element.style.width = '300px';
 */
 
+let currentInterval;
+
 function closeModal(){
     const element = document.querySelector('.overlay');
     element.classList.add('hidden');
+    clearInterval(currentInterval);
 }
 
 function openModal(){
     const element = document.querySelector('.overlay');
     element.classList.remove('hidden');
+    progress(0);
 }
+
+const element = document.querySelector('.bar');
+function progress(step) {
+	if (step > 100) step = 0;
+    element.style.color = 'red';
+    element.style.width = step + '%';
+    currentInterval = setTimeout(progress, 200, step += 1);
+}
+
