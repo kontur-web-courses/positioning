9a997cad870186a1b371b167458728c20558d59c
NoSteam1Bazeev <damir240402@mail.ru>
2022-04-04 14:54:05 +0500

    3, 4

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const bar = document.querySelector('.bar');
+let i = 0;
+
+function update() {
+    bar.style.width = i.toString() + 'px';
+    i+=2;
+    if(i < 201)
+        setTimeout(update, 30);
+}
+
+update();
