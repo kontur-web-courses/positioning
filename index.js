adc9eb7a7961811f9725a5cea687b8b080d2dc95
Ilya <zdravulin@gmail.com>
2022-04-04 19:47:54 +0500

    asdsaafd

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
-
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function move() {
+    const elem = document.querySelector(".progress-bar");
+    let width = 1;
+    const id = setInterval(frame, 1);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
