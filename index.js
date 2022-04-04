948879c5b4f3f99f682751deecf9f054aac7d8f9
NitroLine <48989608+NitroLine@users.noreply.github.com>
2022-04-04 17:28:19 +0500

    Add modal

--- index.js
+++ index.js
@@ -5,3 +5,13 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function closeModal(){
+    const element = document.querySelector('.overlay');
+    element.classList.add('hidden');
+}
+
+function openModal(){
+    const element = document.querySelector('.overlay');
+    element.classList.remove('hidden');
+}
