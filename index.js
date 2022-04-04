9e4a0d30ee2f1e0bd14672ffdea4c4c3ea837cb3
Aleksey <a.e.sokolkov@gmail.com>
2022-04-04 19:28:25 +0500

    1-4

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function extendBar() {
+    let currentWidth = 0;
+    setInterval(addWidth, 30);
+    function addWidth() {
+        if (currentWidth >= 100) {
+            clearInterval(extendBar);
+            document.querySelector('.lb-b').style.display = 'inline-block';
+        } else {
+            currentWidth++;
+            document.querySelector('.progressBar').style.width = currentWidth + '%';
+        }
+    }
+}
+
+extendBar();
