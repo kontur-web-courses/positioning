4820ad46e8c55c490957aaf3ce9b2f71503f31c4
EGREX <kirilmikov591@gmail.com>
2022-04-04 18:46:11 +0500

    оно вроде работает, но я не уверен

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
-
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+let startWidth = 0;
+let maxWidth = 200;
+let intervalId;
+function close_modal(){
+    const element = document.querySelector('.lb-container');
+    element.style.display = 'None';
+    startWidth = 0;
+    clearInterval(intervalId)
+}
+function open_modal(){
+    const element = document.querySelector('.lb-container');
+    element.style.display = "flex";
+    intervalId = setInterval(() => {
+        document.querySelector(".progress_finished").style.width = startWidth + "px";
+        if (startWidth >= maxWidth) {
+            clearInterval(intervalId);
+        }
+        startWidth += 1;
+    }, 10
+    );
+}
