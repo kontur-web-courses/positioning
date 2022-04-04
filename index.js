66588d745a2a2e1368e071c4211674616961dda0
imosk72 <imoskovchenko72@gmail.com>
2022-04-04 17:40:59 +0500

    2 3 4

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function closeLightbox() {
+    document.getElementById("overlay").style.setProperty("display", "none");
+}
+
+let timeInterval = 3000
+let currentWidth = 0;
+let maxWidth = 400;
+let intervalId = setInterval(() => {
+        document.getElementById("progressBarFinished").style.width = ++currentWidth + "px";
+        if (currentWidth >= maxWidth) {
+            clearInterval(intervalId);
+        }
+    }, timeInterval / maxWidth
+);
