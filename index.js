c3072335385a9893d3a94d2ee01e3b9d96fcc9b0
Артём <a.burgart02@gmail.com>
2022-04-04 13:08:59 +0500

    Changes by SomeUnknown

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var width = 0;
 window.onload = function(e){
     setInterval(function () {
         width = width >= 100 ? 0 : width + 10;
-        document.getElementById('progress-bar').style.width = width + '%'; }, 300);
+        document.getElementById('progress-bar').style.width = width + '%';
+        }, 300);
 }
