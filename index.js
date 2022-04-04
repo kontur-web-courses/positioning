1d0c4d4235cf65082f07a7194099b4538b12aa73
Vladik74 <vlad06012002@mail.ru>
2022-04-04 19:49:25 +0500

    CUM

--- index.js
+++ index.js
@@ -5,3 +5,38 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const lightbox = document.querySelector('.lightbox');
+const overlay = document.querySelector('.overlay')
+
+function closeWindow(){
+    lightbox.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+function openWindow(){
+    lightbox.style.display = 'inline-block';
+    overlay.style.display = 'flex';
+}
+
+const closeLightbox = document.querySelector('.closeLightbox');
+closeLightbox.addEventListener('click', closeWindow)
+
+const openLightbox = document.querySelector('.openLightbox');
+openLightbox.addEventListener('click', openWindow);
+
+function animateProgressBar(){
+    document.querySelectorAll('.bar').forEach(progressbar => {
+        let progress = 0;
+
+        const intervalId = setInterval(() => {
+            if (progress < 100) {
+                progress++;
+                let progressLine = progressbar.firstElementChild;
+                progressLine.style.width = `${progress}%`;
+            } else {
+                clearInterval(intervalId)
+            }
+        }, 30);
+    })
+}
