9a331351d0ca7bb97547d660aec7eb730e0e8ca4
PavelLovygin <55824376+PavelLovygin@users.noreply.github.com>
2022-04-04 18:07:31 +0500

    Мизурова Дарья и Ловыгин Павел

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+function mem() {
+    console.log("mem")
+    let count = 0;
+    let progress = setInterval(() =>
+    {
+        console.log("интервал")
+        document.querySelector('.red-line').style.width = `${count++}%`;
+        if (count > 100)
+            clearInterval(progress);
+    }, 30);
+}
+
+mem();
