60ef0e0073c0942a4fb7398bd19e47903f99f5a3
innaanikina <avril-avril@mail.ru>
2022-04-04 19:50:51 +0500

    tasks 1-2 done, tasks 3-4 partially done

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 /*
     Изменить элементу цвет и ширину можно вот так:
 
-    const element = document.querySelector('.myElement');
+    const element =
+    ;
     element.style.color = 'red';
     element.style.width = '300px';
 */
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let openButton = document.querySelector('.open-modal');
+openButton.onclick = function() {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+}
+
+let closeButton = document.querySelector('.close_window');
+closeButton.onclick = function() {
+    modal.style.display = "none";
+    overlay.style.display = "none";
+}
+
+
+
+
+let progressbar = document.querySelector('#progressbar'),
+    max = progressbar.attr('max'),
+    time = (1000/max)*5,
+    value = progressbar.val();
+
+let loading = function() {
+    value += 1;
+    addValue = progressbar.val(value);
+
+    document.querySelector('.progress-value').html(value + '%');
+
+    if (value === max) {
+        clearInterval(animate);
+    }
+};
+let animate = setInterval(function() {
+    loading();
+}, time);
