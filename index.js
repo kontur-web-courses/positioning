3acdcca875689e11df1fc621f508b8ec01e92f00
niripsa <anna.poluyanenko@gmail.com>
2022-04-04 12:51:43 +0500

    end:)

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function closeLightbox() {
+    let lightbox = document.querySelector('.modal-content')
+    lightbox.style.display = 'none';
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'none';
+}
+
+let progress = document.querySelector(".progress");
+let speed = 200;
+let max = 600;
+let value = 0;
+let progress_bar = setInterval(()=>{
+    value += max/speed;
+    progress.style.width = String(value +"px");
+},3000/speed);
