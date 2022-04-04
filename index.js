72e88573999436a5d98b1390092662f36f28e4bc
Axonny <kawaiiaxonny@gmail.com>
2022-04-04 14:33:37 +0500

    progressbar and JS

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+
+document.querySelector('.modal_close').addEventListener('click',  () => {
+    document.querySelector('.modal-container').classList.toggle('hidden');
+});
+
+let progressCount = 0;
+let progress = setInterval(() => {
+    document.querySelector('.progressbar_foreground').style.width = `${progressCount++}%`;
+    if (progressCount > 100) {
+        clearInterval(progress);
+    }
+}, 100);
