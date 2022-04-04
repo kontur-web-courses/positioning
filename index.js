b541a89a262384a899234bd67b62ac7192c36be8
Iakimov Sergey <serg.n.y@yandex.ru>
2022-04-04 17:03:34 +0500

    add 2

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const modal = document.getElementById("modal-window");
+const btn = document.getElementById("modal-button");
+const span = document.getElementsByClassName("modal-close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
