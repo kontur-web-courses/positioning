a6483fcbb94b8e3ff06b4504c392326b59ebba39
Артём <a.burgart02@gmail.com>
2022-04-04 13:10:36 +0500

    3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var width = 0;
 window.onload = function(e){
     setInterval(function () {
-        width = width >= 100 ? 0 : width + 10;
+        width = width >= 100 ? 0 : width + 1;
         document.getElementById('progress-bar').style.width = width + '%';
-        }, 300);
+        }, 30);
 }
