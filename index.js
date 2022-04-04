eec34d9bf9e59374402e0ede9a6166fccc4ec1b0
timpo <timpo@mail.ru>
2022-04-04 14:13:36 +0500

    kek

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+let startWidth = 0;
+let maxWidth = 200;
+let intervalId;
+intervalId = setInterval(() => {
+        document.querySelector(".progress_finished").style.width = startWidth + "px";
+        if (startWidth === maxWidth) {
+            clearInterval(intervalId);
+        }
+        startWidth += 1;
+    }, 25
+);
+
