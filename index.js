892fe218d38f13af390a6d7dbd5625f5f9ff0c9b
Cyber-Frosty <72563369+Cyber-Frosty@users.noreply.github.com>
2022-04-04 17:49:07 +0500

    Progress bar

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+let elem = document.getElementById("load");
+elem.style.width = '0';
+let width = 0;
+let id = setInterval(frame, 7.5);
+function frame() {
+    if (width >= 100) {
+        clearInterval(id);
+        elem.style.backgroundColor = '#00FF00';
+        let text = document.getElementById("progress");
+        text.textContent = 'complete!'
+    } else {
+        width += 0.25;
+        elem.style.width = width + "%";
+    }
+}
