8dcbd2936cbee1592515c69373e00269d3856cce
xyoouy <egorloparev@gmail.com>
2023-04-03 15:16:16 +0500

    Tmp 2

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ function move() {
     let elem = document.getElementById("myBar");
     let width = 0;
     let id = setInterval(frame, 10);
+    document.getElementById("moveButton").disabled = true;
+
     function frame() {
         if (width >= 100) {
+            document.getElementById("moveButton").disabled = false;
             clearInterval(id);
         } else {
             width += 0.5;
             elem.style.width = Math.round(width) + '%';
-            elem.innerHTML = Math.round(width) * 1  + '%';
+            elem.innerHTML = 'Loading...';
         }
     }
 }
