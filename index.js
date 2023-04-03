1346dfcfcaac5c9f120df94f3dcf8fe560eb627d
Valitova Alina <valitovaalina1501@gmail.com>
2023-04-03 15:54:25 +0500

    final

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+const lightbox = document.querySelector('.lightbox');
+const overlay = document.querySelector('.overlay')
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function closeWindow(){
+    lightbox.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+function openWindow(){
+    lightbox.style.display = 'inline-block';
+    overlay.style.display = 'flex';
+}
+
+const closeLightbox = document.querySelector('.closeLightbox');
+closeLightbox.addEventListener('click', closeWindow)
+
+const openLightbox = document.querySelector('.openLightbox');
+openLightbox.addEventListener('click', openWindow);
+
+function progressBar(){
+    document.querySelectorAll('.bar').forEach(progressbar => {
+        let p = 0;
+        const time = setInterval(() => {
+            if (p < 100) {
+                p++;
+                let progressLine = progressbar.firstElementChild;
+                progressLine.style.width = `${p}%`;
+            } else
+                clearInterval(time)
+        }, 30);
+    })
+}
