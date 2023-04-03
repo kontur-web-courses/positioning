2ce1341505e2d250ddb64c26c6083cbafe4a9b7c
natosafix <92174064+natosafix@users.noreply.github.com>
2023-04-03 15:29:13 +0500

    2 task

--- index.js
+++ index.js
@@ -5,3 +5,13 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function openLightbox() {
+    document.getElementById("blur_panel").style.display = "block";
+    document.getElementById("lightbox").style.display = "block";
+}
+
+function closeLightbox() {
+    document.getElementById("blur_panel").style.display = "none";
+    document.getElementById("lightbox").style.display = "none";
+}
