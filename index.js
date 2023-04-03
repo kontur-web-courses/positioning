ce86e2342ac759dd867525d2fdee4cfaf39c7112
ArinaBalakina <Balakinaarina@yandex.ru>
2023-04-03 15:54:25 +0500

    Final

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+
+function closeBox() {
+    document.getElementById("overlay").style.setProperty("display", "none");
+}
+
+let timeInterval = 3000
+let currentWidth = 0;
+let maxWidth = 640;
+let intervalId = setInterval(() => {
+    document.getElementById("progressBarEnd").style.width = ++currentWidth + "px";
+    if (currentWidth >= maxWidth) {
+        clearInterval(intervalId);
+    }
+}, timeInterval / maxWidth
+);
