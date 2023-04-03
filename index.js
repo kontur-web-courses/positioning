73bbc82affafadc735f32cf9d1aaca6cc7852422
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2023-04-03 15:53:07 +0500

    с

--- index.js
+++ index.js
@@ -5,3 +5,17 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const element = document.querySelector('.red-bar');
+element.style.transitionDuration = '0';
+element.style.width = '0';
+function progresbar(){
+    element.style.transitionDuration = '3000ms';
+    element.style.width = '94.1%';
+}
+
+document.querySelector('.close').addEventListener('click', function (){
+    const wrapper = document.querySelector('.lightboxWrapper')
+    wrapper.style.display = 'none';
+})
+
+progresbar();
