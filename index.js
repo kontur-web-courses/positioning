73d74f686d884df13020a2e2604f5c3dcbfa61fc
Danil_Titarenko <danil.titarenko@gmail.com>
2023-04-03 15:30:48 +0500

    4.1

--- index.js
+++ index.js
@@ -5,3 +5,13 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const progress = document.querySelector('.progress');
+const text = document.querySelector('.progress-bar-text');
+let value = 0;
+let max = 600;
+let speed = 200;
+let progressBar = setInterval(() => {
+    value += max / speed;
+    progress.style.width = String(value + "px");
+}, 3000 / speed);
