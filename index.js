23468a6474001ce436c5d9448b8d26ae46fd0f0f
RedSquid2212 <Angelina.Shmantsar@urfu.me>
2023-04-03 17:56:24 +0500

    All tasks done

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+function move() {
+    let elem = document.getElementById("myBar");
+    let width = 0;
+    let id = setInterval(frame, 10);
+    document.getElementById("moveButton").disabled = true;
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+    function frame() {
+        if (width >= 100) {
+            document.getElementById("moveButton").disabled = false;
+            clearInterval(id);
+        } else {
+            width += 0.5;
+            elem.style.width = Math.round(width) + '%';
+            elem.innerHTML = ' ';
+        }
+    }
+}
