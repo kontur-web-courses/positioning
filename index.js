633690ecf4093b5e5d3d27343b97afb5bc85d123
Nikiforov Konstantin <kostya.nik.3854@gmail.com>
2023-04-03 17:49:09 +0500

    task 4

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function hide() {
     lightboxContent.style.display = 'none';
     blur.style.display = 'none';
 }
+
+const fps = 50;
+const animationDuration = 2;
+const cadrLength = 1000/fps;
+const cadrs = fps * animationDuration;
+
+const interval = setInterval(() => {
+    const progress = document.querySelector('progress');
+    progress.value += 100/cadrs;
+    if (progress.value === 100)
+    {
+        clearInterval(interval);
+    }
+}, cadrLength);
