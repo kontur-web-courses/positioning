d0f562b767b5674bba2fbad9ed4be580b2d3ccd0
xyoouy <egorloparev@gmail.com>
2023-04-03 14:56:09 +0500

    3-4 Done

--- index.js
+++ index.js
@@ -8,15 +8,15 @@
 
 function move() {
     let elem = document.getElementById("myBar");
-    let width = elem.style.width;
+    let width = 0;
     let id = setInterval(frame, 10);
     function frame() {
         if (width >= 100) {
             clearInterval(id);
         } else {
-            width++;
-            elem.style.width = width + '%';
-            elem.innerHTML = width * 1  + '%';
+            width += 0.5;
+            elem.style.width = Math.round(width) + '%';
+            elem.innerHTML = Math.round(width) * 1  + '%';
         }
     }
 }
