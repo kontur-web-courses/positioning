acc6f44b9264679da7d669854728670fa79420fb
Dyrda Lev <akronman0000@gmail.com>
2023-04-03 15:56:31 +0500

    Выполненное задание. Дырда Лев, Окулова Ольга

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const progressBar = document.querySelector('.progress');
+let w = 1;
+let id = setInterval(frame, 33);
+function frame() {
+    if (w >= 100) {
+        clearInterval(id);
+    } else {
+        w++;
+        progressBar.style.width = w + '%';
+    }
+}
