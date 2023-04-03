373f6be25c695815f16099e435ce51d0a15f9a09
PavelLovygin <55824376+PavelLovygin@users.noreply.github.com>
2023-04-03 18:49:30 +0500

    add other tasks

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+var overlay = document.querySelector(".overlay");
+var modal = document.querySelector(".modal");
+var closeModalBtn = document.querySelector(".close");
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+}
+
+closeModalBtn.onclick = function() {
+    modal.style.display = "none";
+    overlay.style.display = "none";
+}
+
+function mem() {
+    console.log("mem")
+    let count = 0;
+    let progress = setInterval(() =>
+    {
+        document.querySelector('.red-line').style.width = `${count++}%`;
+        if (count > 100)
+            clearInterval(progress);
+    }, 30);
+}
+
+mem();
