603ebd2983c27bd069d7ec66f7a44b3b88650e09
AndreyKuklinov <Andreydolphin@mail.ru>
2023-04-03 18:15:34 +0500

    sick progress bar

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+// const progressBar = document.querySelector('.progress');
+// let progress = 0;
+
+// const interval = setInterval(() => {
+//   progress += 10;
+//   progressBar.style.width = `${progress}%`;
+//   if (progress >= 100) {
+//     clearInterval(interval);
+//   }
+// }, 1000);
