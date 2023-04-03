f4f9eb4f637cc78a30e2589d6c664dbfc46655fd
senyapekus <ksenya.pshenichnikova03@gmail.com>
2023-04-03 15:54:58 +0500

    1-4

--- index.js
+++ index.js
@@ -5,3 +5,38 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+let start = 0;
+let progressWidth = 200;
+const progressBar = document.querySelector('.progress_finished');
+let modal = document.querySelector(".ligthbox");
+// Get the button that opens the modal
+let btn = document.getElementById("openBtn");
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "flex";
+    let intervalId = setInterval(() => {
+        progressBar.style.width = start + "px";
+
+        if (start === progressWidth) {
+            clearInterval(intervalId);
+        }
+
+        start++;
+    }, 15);
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+    start = 0;
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+        start = 0;
+    }
+}
+
