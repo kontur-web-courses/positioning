58c374a5f084a7c4f9f89aeb825245f1781b74ef
3a4eM-MHE-HuK <gavrilov.nikita@urfu.me>
2023-04-03 17:40:37 +0500

    done all

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+document.querySelectorAll('.progressbar').forEach(progressbar => {
+    let progress = 0
+    const intervalId = setInterval(() => {
+        if (progress < 100) {
+            progress++
+            let progressLine = progressbar.firstElementChild
+            progressLine.style.width = `${progress}%`
+        } else {
+            clearInterval(intervalId)
+        }
+    }, 30)
+})
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+document.querySelectorAll('.modal__close').forEach(closeButton => {
+    closeButton.addEventListener('click', () => closeModal())
+})
+
+
+document.querySelectorAll('.modal__button').forEach(closeButton => {
+    closeButton.addEventListener('click', () => closeModal())
+})
+
+document.querySelectorAll('.open_modal__button').forEach(openButton => {
+    openButton.addEventListener('click', () => openModal())
+})
+
+function closeModal() {
+    document.querySelectorAll('.modal__container').forEach(overlay => overlay.style.display = 'none')
+}
+
+function openModal() {
+    document.querySelectorAll('.modal__container').forEach(overlay => overlay.style.display = 'block')
+}
