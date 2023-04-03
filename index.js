9fd572a60593441d36924ce73541a3d095622676
timofeus <byz0vtm@gmail.com>
2023-04-03 17:41:18 +0500

    dasd

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+var overlay = document.querySelector(".overlay");
+var modal = document.querySelector(".modal");
+var closeModalBtn = document.querySelector(".close");
+
+openModalBtn.onclick = function() {
+  modal.style.display = "block";
+  overlay.style.display = "block";
+}
+
+closeModalBtn.onclick = function() {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == overlay) {
+    modal.style.display = "none";
+    overlay.style.display = "none";
+  }
+}
