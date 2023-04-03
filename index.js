c1d2e8c8f21451a9fa3aee29caed67703d43a563
Pohotlivi Ded <wolf.alex.shishkin@gmail.com>
2023-04-03 18:07:13 +0500

    dadasda

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+function close() {
+    let lightbox = document.querySelector('.overlay')
+    lightbox.style.display = 'none';
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'none';
+}
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+let progress = document.querySelector(".progress_inner");
+let dur = 3000;
+let value = 0;
+let progress_bar = setInterval(()=>{
+    value += 1/3;
+    progress.style.width = String(value +"%");
+    if (value > 100)
+        clearInterval(progress_bar);
+},10);
