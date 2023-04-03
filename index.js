0154452554e4c11f72a423f3d94b3cf2102ebcf2
nnvslv <annavromanovna@gmail.com>
2023-04-03 17:47:53 +0500

    progress bar

--- index.js
+++ index.js
@@ -5,3 +5,30 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function closeLightbox() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("lightbox").style.display = "none";
+}
+
+const progressBar = document.querySelector('.progress-bar');
+const progressText = document.querySelector('.progress-text');
+
+let progress = 0;
+const totalFrames = 60;
+const intervalTime = 3000 / totalFrames;
+
+const animateProgressBar = () => {
+    progress += 100 / totalFrames;
+    progressBar.style.width = `${progress}%`;
+
+    if (progress >= 50) {
+        progressText.style.color = 'white';
+    }
+
+    if (progress < 100) {
+        setTimeout(animateProgressBar, intervalTime);
+    }
+};
+
+setTimeout(animateProgressBar, intervalTime);
