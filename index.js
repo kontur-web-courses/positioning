ae9d8891ceb8ed1df431a4f2f3699497daa68081
andrei-uni <92112053+andrei-uni@users.noreply.github.com>
2023-04-03 15:30:24 +0500

    3

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function startProgress () {
+    let bar = document.querySelector(".bar");
+
+    const increaseBy = 2;
+    let counter = 1;
+
+    function increase() {
+        bar.style.width = increaseBy * counter + '%';
+        counter++;
+
+        if (counter === 20) {
+            clearInterval(intervalId);
+        }
+    }
+
+    let intervalId = setInterval(increase, 500);
+}
