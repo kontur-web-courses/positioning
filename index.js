800854e081a01e2ce600771e1bd631422179d40a
xyoouy <egorloparev@gmail.com>
2023-04-03 14:51:21 +0500

    Tmp

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function move() {
+    let elem = document.getElementById("myBar");
+    let width = elem.style.width;
+    let id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+            elem.innerHTML = width * 1  + '%';
+        }
+    }
+}
