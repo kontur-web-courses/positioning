edf0ec1be75a629bf4ce41433184efc95ede46e9
MelkovaSona <92092128+MelkovaSona@users.noreply.github.com>
2023-04-03 17:54:17 +0500

    GetBR

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 /*
     Изменить элементу цвет и ширину можно вот так:
-
     const element = document.querySelector('.myElement');
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function closeLightbox() {
+    let lightbox = document.querySelector('.modal-content')
+    lightbox.style.display = 'none';
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'none';
+}
+
+let progress = document.querySelector(".progress");
+let speed = 200;
+let max = 600;
+let value = 0;
+let progress_bar = setInterval(()=>{
+    value += max/speed;
+    progress.style.width = String(value +"px");
+},3000/speed);
