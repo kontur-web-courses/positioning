5d933089d4d68269c835abb5642a09fa87728f1c
kolya1601 <saddance7@gmail.com>
2023-04-03 17:32:50 +0500

    .

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-btn');
+
+function openModal() {
+    overlay.style.display = 'block';
+}
+
+function closeModal() {
+    overlay.style.display = 'none';
+}
+
+overlay.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
+modal.addEventListener('click', function(event) {
+    event.stopPropagation(); // предотвращаем закрытие окна при клике внутри него
+});
