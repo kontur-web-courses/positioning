0e95acb60078af8f05ea12e5d5efae98ec83d785
Fedor <pichuginf2016@mail.ru>
2023-04-03 15:39:48 +0500

    4

--- index.js
+++ index.js
@@ -5,3 +5,26 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function fillProgressBar() {
+    const progressBarInner = document.querySelector('.progress-bar-inner');
+    // const progressBarText = document.querySelector('.progress-bar-text');
+    const duration = 3000;
+    const frameRate = 60;
+    const frameDuration = duration / frameRate;
+    let progress = 0;
+
+    const increaseProgress = () => {
+        progress += 100 / frameRate;
+        progressBarInner.style.width = `${progress}%`;
+        // progressBarText.textContent = `Loading... ${Math.round(progress)}%`;
+
+        if (progress < 100) {
+            setTimeout(increaseProgress, frameDuration);
+        }
+    };
+
+    increaseProgress();
+}
+
+fillProgressBar();
