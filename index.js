8b2caac39898f75b0b85b540bbd1fd460bf96c2c
D3RPole96 <ilya.lagunov2003@gmail.com>
2023-04-03 20:53:08 +0500

    Finale

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+function move() {
+    let bar = document.getElementById("myBar");
+    let barText = document.getElementById("myText");
+    let width = 1;
+    let id = setInterval(frame, 10);
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        }
+        else {
+            width++;
+            bar.style.width = width + '%';
+            barText.style.clipPath = `polygon(0 0, ${width}% 0, ${width}% 100%, 0% 100%)`
+        }
+    }
+}
+
+move();
