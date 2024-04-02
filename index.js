3f1835474e856b48919621904b06020523d4a6a2
fuwariko <cessedimeow@gmail.com>
2024-04-02 18:20:01 +0500

    add

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
-
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+const element = document.querySelector('.progress .bar .progress-part');
+element.style.color = 'red';
+let i = 0;
+setInterval(function() {
+    element.style.width = `${i++}px`;
+}, 3000/500)
