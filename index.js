00e04c21712036b37c60cdbc1a125c50749f1e9c
Vearsmon <vearsmon285537@gmail.com>
2024-04-02 19:20:22 +0500

    123

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+
+const progressBar = document.querySelector('.progress-fill');
+
+function updateProgressBar(value) {
+progressBar.style["clip-path"] = `inset(0 ${100 - value}% 0 0)`;
+}
+
+// Пример использования функции updateProgressBar():
+let i = 0;
+var interval = setInterval(function() {
+    if (i < 100) {
+      i += 1;
+      updateProgressBar(i);
+    }
+  }, 50);
