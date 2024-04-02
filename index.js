463b28723c273aace27fac49addbeb995f2a0374
Evgeny Lapshin <zhenia.lapshin201@yandex.ru>
2024-04-02 19:13:01 +0500

    added accordeon

--- index.js
+++ index.js
@@ -26,3 +26,11 @@ let iter = () => {
     } 
 }
 setTimeout(iter, 30)
+
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
