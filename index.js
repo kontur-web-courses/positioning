a9688c72c2a7ac846d4e0283a5046b1be772c6ce
cowabangerrrrr <uzaninarsenij28@gmail.com>
2024-04-02 18:12:26 +0500

    4 task done

--- index.js
+++ index.js
@@ -5,3 +5,17 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function animateBar() {
+    const progressBar = document.querySelector('.bar');
+
+    let progress = 0;
+    const id = setTimeout(() => {
+        if (progress < 100) {
+            progress++;
+            progressBar.firstElementChild.style.width = `${progress}%`
+        }
+    })
+}
+
+animateBar();
