01da889eff96c6a62315bd82ab0758b4c2b43b8b
BigMommaPr <egoregormart09@gmail.com>
2024-04-02 18:01:26 +0500

    main task complete

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+const modal = document.getElementById("modalWindow");
+const modalButton = document.getElementById("openModal");
+const span = document.getElementsByClassName("closeButton")[0];
+
+modalButton.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const progressBar = document.querySelector(".progress");
+    const loadingText = document.querySelector(".loading-text");
+
+    let progress = 0;
+    progressBar.style.width = progress + "%";
+
+    function animateProgress() {
+        const interval = setInterval(function() {
+            progress++;
+            progressBar.style.width = progress + "%";
+            loadingText.textContent = "Loading... " + progress + "%";
+
+            if (progress >= 100) {
+                clearInterval(interval);
+                loadingText.textContent = "complete";
+            }
+        }, 30);
+    }
+
+    animateProgress();
+});
+
 /*
     Изменить элементу цвет и ширину можно вот так:
 
