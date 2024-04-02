7f5a9b345268bad8c5c98c6c9ee2fc83871fc054
magnus5552 <isackov.stepa@yandex.ru>
2024-04-02 19:06:12 +0500

    3-4 done

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const progressBar = document.querySelector(".progress-bar");
+
+function fillProgressBar(pb) {
+    pb.style.animation = '3s ease 0s 1 normal forwards running fillPb';
+    setTimeout(() => clearProgressBar(pb), 3000)
+}
+
+function clearProgressBar(pb) {
+    pb.style.animation = null;
+}
+
+setInterval(() => fillProgressBar(progressBar), 3500)
