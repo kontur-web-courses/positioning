404be87e8562e57644294f6e1f57a3599e35fd84
Kol1n13 <60143734+Kol1n13@users.noreply.github.com>
2024-04-02 18:50:50 +0500

    progress-bar

--- index.js
+++ index.js
@@ -9,10 +9,28 @@
 function func(){
     const element = document.getElementById('modal');
     element.style.visibility = 'visible';
+    progressTimer();
 }
 
 
 function func1(){
     const element = document.getElementById('modal');
     element.style.visibility = 'hidden';
+    const elem2 = document.getElementById('red');
+    elem2.style.clipPath = "clip-path: polygon(0 0, 0, 0, 0, 100%, 0% 100%)";
 }
+
+function progressTimer(){
+    let width = 0;
+    let progressBar = document.getElementById('red');
+    let time = setInterval(progress, 30);
+
+    function progress(){
+        progressBar.style.clipPath = `polygon(0 0, ${width}% 0, ${width}% 100%, 0% 100%)`;
+        width++;
+        if (width == 101){
+            clearInterval(time);
+        }
+    }
+}
+
