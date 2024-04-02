ff93e03125b0d8b89a995b5a3eb72684db89e8c3
n1gtm <40951780+gx56q@users.noreply.github.com>
2024-04-02 17:53:46 +0500

    js for task 2

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const lightbox = document.querySelector('.lightbox');
+const overlay = document.querySelector('.overlay')
+
+function closeWindow(){
+    lightbox.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+function openWindow(){
+    lightbox.style.display = 'inline-block';
+    overlay.style.display = 'flex';
+}
+
+const closeLightbox = document.querySelector('.closeButton');
+closeLightbox.addEventListener('click', closeWindow)
+
+const openLightbox = document.querySelector('.openButton');
+openLightbox.addEventListener('click', openWindow);
