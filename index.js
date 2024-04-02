096f36f5ad866e25e806207bbd29aaac0ef87429
everlast1ngw1nter <at07012004@gmail.com>
2024-04-02 18:26:03 +0500

    task4

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ document.getElementById('openOverlayButton').addEventListener('click', function(
 document.querySelector('.closeButton').addEventListener('click', function() {
     document.querySelector('.overlay').style.display = 'none';
 });
+
+const progressBar = document.querySelector('.waiting');
+
+function animateProgressBar(duration = 3000) {
+    let start = null;
+
+    function step(timestamp) {
+        if (!start) {
+            start = timestamp;
+        }
+        const progress = timestamp - start;
+        const progressPercentage = Math.min(progress / duration, 1);
+        progressBar.style.width = `${progressPercentage * 100}%`;
+        if (progressPercentage < 1) {
+            window.requestAnimationFrame(step);
+        }
+    }
+    window.requestAnimationFrame(step);
+}
+
+animateProgressBar();
