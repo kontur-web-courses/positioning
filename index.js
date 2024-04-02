d26c4127aadcbeaafd285a0afe9a4734068ae0eb
unluckyLuka <Roman.Khairutdinov@urfu.me>
2024-04-02 13:24:24 +0500

    2task
    meow2

--- index.js
+++ index.js
@@ -5,3 +5,12 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.close');
+
+closeButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+});
