80e5880a4165cdc01c230d5cc9628f03c6d161df
IlyaTucha <atuchin04@yandex.ru>
2024-04-02 17:47:18 +0500

    second ex

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+
+const modal = document.getElementById("myModal");
+const closeBtn = document.querySelector(".close");
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
