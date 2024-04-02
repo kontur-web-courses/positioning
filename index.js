cab7a841c385127eb5dc347494876159a2780ac1
hiNEt0 <hinet.f23@gmail.com>
2024-04-02 17:49:47 +0500

    second task done

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.modal-overlay');
+    const closeButton = document.querySelector('.close-btn');
+
+    function openModal() {
+        modal.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+
+    function closeModal() {
+        modal.style.display = 'none';
+        overlay.style.display = 'none';
+    }
+
+    closeButton.addEventListener('click', closeModal);
+
+    openModal();
+});
