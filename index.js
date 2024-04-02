112b18962ad42b5a5c14938a95a9bc6213fb8846
Luzgina <luzgina-ksyu@mail.ru>
2024-04-02 18:42:57 +0500

    пушка бомба петарда

--- index.js
+++ index.js
@@ -5,3 +5,9 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const element = document.querySelector('.progress .bar .progress-part');
+element.style.color = 'red';
+let i = 0;
+setInterval(function () {
+    element.style.width = `${i++}px`;
+}, 3000 / 640)
