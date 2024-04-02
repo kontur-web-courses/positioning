ea3a3ceda70feeb66412c5345ec0803a3ceff2f4
yellooot <njkzrhjkz@gmail.com>
2024-04-02 18:20:38 +0500

    some for 3

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function close(){
     const modal = document.querySelector('.modal');
     modal.style.display = 'none';
 }
+
+const fps = 50;
+const animationDuration = 2;
+const cadrLength = 1000/fps;
+const cadrs = fps * animationDuration;
+
+const interval = setInterval(() => {
+    const progress = document.querySelector('progress');
+    progress.value += 100/cadrs;
+    if (progress.value === 100)
+    {
+        clearInterval(interval);
+    }
+}, cadrLength);
