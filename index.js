a6c0c885bb027df23299a97ebcb72f60feb15559
antoshkaxxr <63500718+antoshkaxxr@users.noreply.github.com>
2024-04-02 17:24:21 +0500

    tasks 1-2

--- index.js
+++ index.js
@@ -5,3 +5,23 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+document.addEventListener('DOMContentLoaded', function() {
+    let modal = document.getElementById('modal');
+    let btn = document.getElementById("openModal");
+    let span = document.getElementById("closeModal");
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
