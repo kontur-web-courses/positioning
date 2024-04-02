5939aee767b097425243d51591003e5325a139d5
Константин <kostss332@gmail.com>
2024-04-02 18:39:47 +0500

    all done

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+function closeLightbox() {
+    let lightbox = document.querySelector('.modal-content')
+    lightbox.style.display = 'none';
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'none';
+}
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+let progress = document.querySelector(".progress");
+let speed = 300;
+let max = 600;
+let value = 0;
+let progress_bar = setInterval(() => {
+    if (value < 590) {
+        value += max / speed;
+        speed += 3;
+        progress.style.width = String(value + "px");
+    }
+}, 3000 / speed);
