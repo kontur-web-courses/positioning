6954deebd025a123c5fbf2511b205a362991847b
Mike <kononov.mixa2045@gmail.com>
2024-04-02 18:45:57 +0500

    2 comp

--- index.js
+++ index.js
@@ -5,3 +5,20 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const lightbox = document.getElementById('lightbox');
+const openButton = document.getElementById('openLightboxButton');
+const lightboxButton = document.getElementsByClassName('lightboxButton');
+const overlay = document.getElementById('overlay');
+
+openButton.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    overlay.style.display = 'block';
+});
+
+for (const lightboxButtonElement of lightboxButton) {
+    lightboxButtonElement.addEventListener('click', () => {
+        lightbox.style.display = 'none';
+        overlay.style.display = 'none';
+    });
+}
