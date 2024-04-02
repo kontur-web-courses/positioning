a8d0fe5c8d24b015e5a84cc45b36d403c5b09ec9
AIM1rage <michael050100@gmail.com>
2024-04-02 19:07:48 +0500

    all tasks done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
-
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+document.querySelectorAll(".progressBar").forEach(progressbar => {
+    let progress = 0;
+    const intervalId = setInterval(() => {
+        if (progress < 100) {
+            progress++;
+            let progressLine = progressbar.firstElementChild;
+            progressLine.style.width = `${progress}%`;
+        } else {
+            clearInterval(intervalId);
+        }
+    }, 30);
+});
