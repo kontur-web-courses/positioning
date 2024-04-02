1a7684177d90547031d96cec81a24bb8df650b3a
vVladEr <vlader.2004.ekb@gmail.com>
2024-04-02 19:30:33 +0500

    add progress bar

--- index.js
+++ index.js
@@ -5,3 +5,23 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        const elem = document.querySelector('.progressBar');
+        var width = 1;
+        var id = setInterval(frame, 30);
+        function frame() {
+            console.log(width)
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.clipPath = `inset(0 0 0 ${width}%)`;
+            }
+        }   
+    }
+}
