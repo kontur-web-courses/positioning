0ce1f3b6d44df5dbbf7dd6cd2ed0599c4f6ee35a
Daemetry <hrudmirom@gmail.com>
2024-04-02 18:14:43 +0500

    Modal...

--- index.js
+++ index.js
@@ -4,17 +4,26 @@
 // element.style.color = 'red';
 // element.style.width = '300px';
 
-const openModalButton = document.getElementById('openModal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.modal-overlay');
+const closeButton = document.querySelector('.close-btn');
+
+// const openModalButton = document.getElementById('openModal');
 const progressModal = document.getElementById('progressModal');
 
 const progressBar = document.getElementById('progress-bar')
 
 // const progressBar = document.querySelector('.progress-bar')
 
-openModalButton.addEventListener('click', () => {
-    progressModal.style.display = 'block';
+document.addEventListener('DOMContentLoaded', function() {
+    // progressModal.style.display = 'block';
 
     progressBar.style.width = '0px';
+
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+        overlay.style.display = 'none';
+    });
 
     let width = 0;
     const interval = setInterval(() => {
