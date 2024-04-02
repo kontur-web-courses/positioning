8825f04e579a4ff0134d7fdd1c005794bc508e9b
bober19 <115493537+Manykeys@users.noreply.github.com>
2024-04-02 17:51:17 +0500

    hi

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function func(){
+    const element = document.getElementById('modal');
+    element.style.visibility = 'visible';
+}
+
+
+function func1(){
+    const element = document.getElementById('modal');
+    element.style.visibility = 'hidden';
+}
