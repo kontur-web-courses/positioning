7eddf9031f4a7bb4131055f16c306f106872818a
Ortem08 <72299038+Ortem08@users.noreply.github.com>
2024-04-02 18:19:50 +0500

    3-4 bro

--- index.js
+++ index.js
@@ -6,6 +6,22 @@
     element.style.width = '300px';
 */
 
+function move() {
+    let elem = document.querySelector(".progress-bar");
+    let width = 0;
+    let id = setInterval(frame, 33);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        }
+        else {
+            width++;
+            elem.style.width = width + "%";
+            elem.innerHTML = "Loading...";
+        }
+    }
+}
+
 document.querySelector('.modal_window_close').onclick = function () {
     document.querySelector('.modal_window').style.display = 'none';
     document.querySelector('.modal_window').style.display = 'none';
