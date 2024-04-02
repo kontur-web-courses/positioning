b64680151a08d529b5e85035cded9e1a980d31c1
ISich <arseny.voroni@yandex.ru>
2024-04-02 18:13:01 +0500

    pre load

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+document.addEventListener('DOMContentLoaded', () => {
+    let progress = 0; 
+    const interval = 30;
+    const duration = 3000;
+    const step = interval / duration * 100;
+  
+    const progressElement = document.getElementById('progress');
+    const progressText = document.getElementById('progress-text');
+  
+    const intervalId = setInterval(() => {
+      progress += step;
+      if (progress >= 100) {
+        progress = 100;
+        clearInterval(intervalId);
+      }
+  
+      progressElement.style.width = `${progress}%`;}, interval);
+  });
+  
