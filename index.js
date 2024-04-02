8ae35b68d03c97dd3f6f21b6ff627c7aa09345e2
Amyton0 <khlopina.sd@gmail.com>
2024-04-02 18:42:53 +0500

    Сделано 1-4

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector(".close");
+
+closeBtn.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+let progress = document.querySelector(".progress");
+let speed = 300;
+let max = 600;
+let value = 0;
+let progress_bar = setInterval(() => {
+        value += max / speed;
+        progress.style.width = String(value + "px");
+}, 3000 / speed);
