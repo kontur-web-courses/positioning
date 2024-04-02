3fccb8ce81d5ca55c6f6e98c03a622d7f2f36e01
Vladimir Boyko <ya.vladimir565@mail.ru>
2024-04-02 18:29:39 +0500

    Task 4

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+function fillProgressBar() {
+    const progressBar = document.querySelector('.progress');
+    progressBar.style.background = 'red';
+    let start = null;
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+        if (start === null) start = timestamp;
+        const progress = timestamp - start;
+        const progressPercentage = progress / 3000;
+
+        progressBar.style.width = `${progressPercentage * 100}%`;
+
+        if (progressPercentage < 1) {
+            window.requestAnimationFrame(step);
+        }
+    }
+}
+
+fillProgressBar();
