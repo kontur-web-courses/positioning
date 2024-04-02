18d3472b92584c6981ceafde5e7f7d9141a98baf
factor-feerist <erik.blodoex@gmail.com>
2024-04-02 15:39:48 +0300

    first task done

--- index.js
+++ index.js
@@ -5,3 +5,6 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+for (const logo of document.querySelectorAll('.logo')) {
+    logo.style.width < logo.style.height ? logo.style.height = '100px' : logo.style.width = '100px';
+}
