a651fbd2dd18daef715012014b50474436f876c8
dimastarsis <kurilin.dmitriy@gmail.com>
2024-04-02 17:47:24 +0500

    2

--- index.js
+++ index.js
@@ -5,3 +5,20 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close');
+
+function openModal() {
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+overlay.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
