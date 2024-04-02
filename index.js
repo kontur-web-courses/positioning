4369a76485acb73ae750bbb929bd2be45d19c54b
hiNEt0 <hinet.f23@gmail.com>
2024-04-02 22:04:26 +0500

    4th task done

--- index.js
+++ index.js
@@ -22,3 +22,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
     closeButton.addEventListener('click', closeModal);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const progressBar = document.querySelector('.progress-bar-active');
+
+    const duration = 3000;
+    const frames = 60;
+    const increment = duration / frames;
+
+    let progress = 0;
+
+    const interval = setInterval(function() {
+        progress += 100 / frames;
+        progressBar.style.clipPath = `inset(0 ${100 - progress}% 0 0)`;
+        if (progress >= 100) {
+            clearInterval(interval);
+        }
+    }, increment);
+});
