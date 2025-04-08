b0ba9751ddef2b34cf0fc0fdc75a64cafd2cbaa3
Firsikita <nikitafirsov191@gmail.com>
2025-04-08 15:57:09 +0500

    1-2

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
+const overlay = document.getElementById('overlay');
+
+openModalBtn.addEventListener('click', () => {
+    overlay.classList.add('active');
+});
+
+closeModalBtn.addEventListener('click', () => {
+    overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', (e) => {
+    if (e.target === overlay) {
+        overlay.classList.remove('active');
+    }
+});
