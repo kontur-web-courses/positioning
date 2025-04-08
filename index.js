66ee1414abe1c574607f26308ef0a800a107aaa4
PashaKorsh <p@korshunov.me>
2025-04-08 19:25:32 +0500

    pmrhoierhol

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const element = document.querySelector('.red');
 
-let a = 0;
+let a = 100;
 let fps = 60;
 let b = setInterval(() => {
-    element.style.width = Math.min(a, 100) + '%';
-    a += 100 / fps / 3;
+    element.style.clipPath = "inset(0 " + Math.min(100, a) + "% 0 0)"
+    a -= 100 / fps / 3;
 }, 1000 / fps);
 setTimeout(() => clearInterval(b), 3000);
