9ee0090e5b8db07eac95cb2dca602e1f5ae7a83f
Robert Malikov <Robert.Malikov@urfu.me>
2025-04-08 19:30:26 +0500

    4

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ closeButton.addEventListener('click', () => {
     const otherPage = document.getElementById('other-page');
     otherPage.style.display = "none";
 })
+
+
+const strip = document.getElementById('strip');
+let perc = 0;
+
+function timePass() {
+    setTimeout(() => {
+        perc++;
+        strip.style.clipPath = `inset(0 ${100 - perc}% 0 0)`;
+        if (perc < 100) {
+            timePass();
+        }
+    }, 10)
+}
+
+
+timePass();
