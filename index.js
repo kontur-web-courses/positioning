0e0c30d8e632af3adc5891850829b60345ee1111
Jakob Ornysh <jornysh@gmail.com>
2025-04-08 18:23:57 +0500

    5

--- index.js
+++ index.js
@@ -9,3 +9,12 @@
 
 const element = document.querySelector('.progress-fill');
 element.style.width = '100%';
+
+const closeButton = document.querySelector('.modal-close');
+closeButton.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.modal-overlay');
+    
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
