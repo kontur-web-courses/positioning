4b8e858237e1ae2443d731924ed0f465b38ee78c
IIIMAXIMIII <145893187+IIIMAXIMIII@users.noreply.github.com>
2025-04-08 18:01:53 +0500

    2

--- index.js
+++ index.js
@@ -5,3 +5,13 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+document.querySelector('.action-btn').addEventListener('click', function() {
+    document.querySelector('.overlay').style.display = 'none';
+});
+
+document.querySelector('.overlay').addEventListener('click', function(e) {
+    if (e.target === this) {
+        this.style.display = 'none';
+    }
+});
