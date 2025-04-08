1d4f0395d4601128cb39a7fd9569a790a77a2184
Alexander Iarmoshenko <a2398253@gmail.com>
2025-04-08 18:22:50 +0500

    task 2

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+
+const close_button = document.querySelector('.close-button');
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+
+close_button.addEventListener('click', () => {
+    modalWindow.style.display = 'none';
+    overlay.style.display = 'none';
+})
+
