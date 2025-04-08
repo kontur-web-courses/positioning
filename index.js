2c3b239f6f80d3bc6947af9713910865fb304679
Jakob Ornysh <jornysh@gmail.com>
2025-04-08 19:05:18 +0500

    white "Loading..."

--- index.js
+++ index.js
@@ -8,7 +8,21 @@
 
 
 const element = document.querySelector('.progress-fill');
+const progressBar = document.querySelector('.progress-bar');
+const whiteText = document.querySelector('.progress-text-white');
+
+function updateWhiteText() {
+    const progressWidth = element.offsetWidth / progressBar.offsetWidth * 100;
+    whiteText.style.clipPath = `inset(0 ${100 - progressWidth}% 0 0)`;
+    
+    if (progressWidth < 100) {
+        requestAnimationFrame(updateWhiteText);
+    }
+}
+
 element.style.width = '100%';
+
+updateWhiteText();
 
 const closeButton = document.querySelector('.modal-close');
 closeButton.addEventListener('click', () => {
