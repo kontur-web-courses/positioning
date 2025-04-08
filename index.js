99dced56f57f0fee23acd69094c506f3c28e6d4b
CtREL0K <Kzuev905@gmail.com>
2025-04-08 17:44:16 +0500

    modal window and logos

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modalOverlay = document.getElementById('modalOverlay');
+const openModalButton = document.getElementById('openModal');
+const closeButton = document.getElementById('closeButton');
+
+function openModal() {
+    modalOverlay.style.display = 'flex';
+}
+
+function closeModal() {
+    modalOverlay.style.display = 'none';
+}
+
+openModalButton.addEventListener('click', openModal);
+closeButton.addEventListener('click', closeModal);
+
+modalOverlay.addEventListener('click', function(event) {
+    if (event.target === modalOverlay) {
+        closeModal();
+    }
+});
