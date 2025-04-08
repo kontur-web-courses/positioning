937c373f8472054280f7dc3fd9601c7510960760
CtREL0K <Kzuev905@gmail.com>
2025-04-08 18:13:03 +0500

    dynamic progress bar

--- index.js
+++ index.js
@@ -9,9 +9,15 @@
 const modalOverlay = document.getElementById('modalOverlay');
 const openModalButton = document.getElementById('openModal');
 const closeButton = document.getElementById('closeButton');
+const progressBar = document.querySelector('.bar');
+const loadingText = document.querySelector('.loading-text');
 
 function openModal() {
     modalOverlay.style.display = 'flex';
+    progressBar.style.width = '0%';
+    loadingText.style.color = '#000';
+
+    animateProgressBar();
 }
 
 function closeModal() {
@@ -26,3 +32,19 @@ modalOverlay.addEventListener('click', function(event) {
         closeModal();
     }
 });
+
+function animateProgressBar() {
+    let width = 0;
+    const baseWidth = 600
+    const intervalId = setInterval(() => {
+        if (width >= baseWidth) {
+            clearInterval(intervalId);
+        } else {
+            if (width >= baseWidth / 2) {
+                loadingText.style.color = '#fff';
+            }
+            width += 1;
+            progressBar.style.width = `${width * 100 / baseWidth}%`;
+        }
+    }, 5);
+}
