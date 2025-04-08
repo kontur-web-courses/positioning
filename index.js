79abb87264533a66ceb7e0292989d976fd76abb3
Andrey <Andreyvol9898@gmail.com>
2025-04-08 18:53:26 +0500

    final

--- index.js
+++ index.js
@@ -15,3 +15,30 @@ document.getElementById('modalOverlay').addEventListener('click', function(event
 document.getElementById('okButton').addEventListener('click', function() {
     document.getElementById('modalOverlay').style.display = 'none';
 });
+
+
+function startProgressAnimation() {
+    const progressBar = document.querySelector('.progress-bar');
+
+    const duration = 3000;
+    const startTime = Date.now();
+
+    function animateProgress() {
+        const currentTime = Date.now();
+        const progress = Math.min(1, (currentTime - startTime) / duration);
+
+        progressBar.style.width = `${progress * 100}%`;
+        progressBar.style.backgroundColor = `rgb(${Math.floor(progress * 255)}, 0, 0)`;
+
+        if (progress < 1) {
+            requestAnimationFrame(animateProgress);
+        }
+    }
+
+    animateProgress();
+}
+
+document.getElementById('openModalBtn').addEventListener('click', function() {
+    document.getElementById('modalOverlay').style.display = 'flex';
+    startProgressAnimation();
+});
