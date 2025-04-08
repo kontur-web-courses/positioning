55ccadaafb90f62c660dbcbc9942bba4524b1e9d
mzukov <Mikhail.Ziukov@urfu.me>
2025-04-08 15:59:35 +0500

    4

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+function animateProgressBar() {
+    const progressBar = document.querySelector('.bar');
+
+    let progress = 0;
+    const interval = setInterval(() => {
+        if (progress < 100) {
+            progress++;
+            progressBar.firstElementChild.style.width = `${progress}%`;
+        } else {
+            clearInterval(interval);
+        }
+    }, 30);
+
+    progressBar.firstElementChild.style.width = '100%';
+
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 3000);
+}
+
+animateProgressBar();
