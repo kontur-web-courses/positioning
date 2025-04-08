3c99c56f8b53760eed56226a45ea55265e3e404f
Grinik767 <grigorikiseleff@yandex.ru>
2025-04-08 18:37:13 +0500

    finish

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
-
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
-
-document.querySelector('.action-btn').addEventListener('click', function() {
+document.querySelector('.action-btn').addEventListener('click', function () {
     document.querySelector('.overlay').style.display = 'none';
 });
 
-document.querySelector('.overlay').addEventListener('click', function(e) {
+document.querySelector('.overlay').addEventListener('click', function (e) {
     if (e.target === this) {
         this.style.display = 'none';
     }
 });
+
+const progressBar = document.querySelector('.progress-fill');
+
+const duration = 3000;
+let startTime = null;
+
+function animate(timestamp) {
+    if (!startTime)
+        startTime = timestamp;
+
+    const elapsed = timestamp - startTime;
+    const progressPercent = Math.min((elapsed / duration) * 100, 100);
+    progressBar.style.width = progressPercent + '%';
+
+    if (elapsed < duration) {
+        window.requestAnimationFrame(animate);
+    }
+}
+
+window.requestAnimationFrame(animate);
