112d771925a5f4ff24f3cdf84751bbda49b493d4
Shutcing <maksim19111@gmail.com>
2025-04-08 18:43:46 +0500

    commit 4

--- index.js
+++ index.js
@@ -5,3 +5,17 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+setTimeout(() => {
+  const progress = document.querySelector(".progress__redWrapper");
+
+  let percentage = 0;
+  let id = setInterval(() => {
+    progress.style.width = `${percentage}%`;
+    percentage += 3;
+    if (percentage > 100) {
+      progress.style.width = `100%`;
+      clearInterval(id);
+    }
+  }, 100);
+}, 100);
