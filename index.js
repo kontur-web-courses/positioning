d66e4106824d23737e941992e8da1634548daeb3
F1Soda <golik.tima@gmail.com>
2025-04-08 16:45:35 +0500

    WE WANT SEX

--- index.js
+++ index.js
@@ -40,3 +40,11 @@ function animateProgressBar() {
     }, 1000 / frameRate);
 }
 
+const headers = document.querySelectorAll('.accordion-header');
+
+headers.forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+});
