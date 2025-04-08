f37f8d101a330a38eb34643d81e9109d57ebffb5
OK <143250301+OK219@users.noreply.github.com>
2025-04-08 18:19:02 +0500

    2

--- index.js
+++ index.js
@@ -5,3 +5,23 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('overlay');
+const closeBtn = document.getElementById('close-btn');
+const openModalBtn = document.querySelector('.open-modal-btn');
+
+function openModal() {
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+    document.body.style.overflow = 'hidden'; 
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+    document.body.style.overflow = '';
+}
+
+openModalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
