23c9f74c0342b4b7d7f6d4c7b700154c91268619
artemchick35 <artemryzhkov2004@gmail.com>
2025-04-08 17:37:09 +0500

    LAAAAAAAAST

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// const element = document.querySelector('.lightbox_progress_bar');
-// element.style.backgroundColor = 'red';
-
-
-
-const element = document.querySelector('.lightbox_progress_bar');
+const element = document.querySelector('.red-progress');
 let startTime = null;
 
 function animateProgress(timestamp) {
@@ -12,19 +7,14 @@ function animateProgress(timestamp) {
     const percent = Math.min(progress / 3000, 1); // 3 секунды = 3000 мс
 
     element.style.width = `${percent * 100}%`;
+    // element.style.color = 'white';
 
     if (percent < 1) {
         requestAnimationFrame(animateProgress);
     }
 }
 
-element.textContent = "Loading...";
 element.style.backgroundColor = 'red';
-element.style.height = '20px';
-element.style.width = '0%';
-//element.style.transition = 'none'; // Отключаем CSS-переходы
-// element.style.textAlign = 'center';
-element.style.color = 'white';
 
 // Запуск анимации
 requestAnimationFrame(animateProgress);
