afa1f074fe1307f161cef673b4fbc58f77af49b5
vladr <vladreva57@gmail.com>
2025-04-08 17:59:21 +0500

    Reva: modal window

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const openLightbox = document.getElementById('openLightbox');
+const closeLightbox = document.getElementById('closeLightbox');
+const overlay = document.getElementById('overlay');
+const modal = document.getElementById('modal');
+
+openLightbox.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+});
+
+closeLightbox.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+});
