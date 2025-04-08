d7e6e153e5c16fbc514a5087597d771f8fc92eb9
Mot1x <motixflawless@gmail.com>
2025-04-08 19:09:08 +0500

    всё

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
+const progress = document.querySelector('.progress-fill');
+const progress_text = document.querySelector('.progress-text-fill');
+let width = 0;
 
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
+const interval = setInterval(() => {
+    width += 1;
+    progress.style.width = width + '%';
+    if (width >= 100) {
+        clearInterval(interval);
+    }
+}, 30);
+
+function modifyModalWindow(visibility){
+    const window = document.querySelector(".lightbox");
+    window.style.visibility = visibility;
+    const overlay = document.querySelector(".lightbox-overlay");
+    overlay.style.visibility = visibility;
+}
+
+function closeModalWindow(){
+    modifyModalWindow("hidden");
+}
+
+function openModalWindow(){
+    modifyModalWindow("visible");
+}
